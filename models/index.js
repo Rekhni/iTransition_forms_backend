6dc61306c35b16d6da7f2bd9572fcd2c84619029
Rakhymzhan <mark.john.99@mail.ru>
2025-04-25 00:44:16 +0500

    updated getAllTemplates controller #2

--- models/index.js
+++ models/index.js
@@ -41,8 +41,8 @@ Template.hasMany(Like, { foreignKey: 'templateId' });
 Like.belongsTo(User, { foreignKey: 'userId' });
 Like.belongsTo(Template, { foreignKey: 'templateId' });
 
-Template.belongsToMany(User, { through: TemplateAccess, foreignKey: 'templateId' });
-User.belongsToMany(Template, { through: TemplateAccess, foreignKey: 'userId' });
+Template.belongsToMany(User, { through: TemplateAccess, foreignKey: 'templateId', as: 'allowedUsers' });
+User.belongsToMany(Template, { through: TemplateAccess, foreignKey: 'userId', as: 'accessibleTemplates' });
 
 const db = {
     sequelize,
