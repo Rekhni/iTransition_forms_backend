c1ec7c118f1941f8e01522d6c0548239160b8448
Rakhymzhan <mark.john.99@mail.ru>
2025-05-11 15:35:38 +0500

    added odoo integration #4

--- routes/odoo.js
+++ routes/odoo.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { pushToOdoo } from '../controllers/odooPushController.js';
 
 const router = express.Router();
-router.get('/push', pushToOdoo);
+router.post('/push', pushToOdoo);
 
 export default router;
